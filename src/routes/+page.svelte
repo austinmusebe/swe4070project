d1849fa930fe19325fb26a7f32b01e54925a4cb4
austinmusebe <austinmusebe@gmail.com>
2025-10-24 11:42:28 +0300

    Updating the file structure, left comments for what we need to do in each file.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="title">Welcome to SvelteKit, Rabira</h1>
+<p class="text-base text-gray-400 p-4">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ src/stores/cart.js
@@ -0,0 +1,1 @@
+// Cart state & products
