ccc8fcd42c04d1c0376a0389613f2fc4229b0f8a
austinmusebe <austinmusebe@gmail.com>
2025-10-25 20:01:52 +0300

    Implemented some header boilerplate.

---
+++ src/components/Header.svelte
@@ -0,0 +1,37 @@
+
+<header>
+    <nav>
+        <ul class="header-content">
+            <li class="business">Bees' Ness</li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Manage Account</a></li>
+            <li><a href="/">Register / Login</a></li>
+        </ul>
+    </nav>
+
+</header>
+<!-- styles -->
+ <style>
+    header {
+        align-items: flex-end;
+        justify-content: flex-end;
+        display: flex;
+        background-color: grey;
+    }
+    .header-content{
+        align-items: flex-end;
+        display: flex;
+        flex-direction: row;
+        padding: 20px;
+        gap: 20px;
+        justify-content: flex-end;
+    }
+    .business{
+        /* padding-right: 87%; */
+        position: absolute;
+        right: 90%;
+        margin-top: .5rem;
+        white-space: nowrap;
+        font-size: 2rem;
+    }
+ </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Header from "../components/Header.svelte";
+
+</script>
+<Header/>
 <h1 class="title">Welcome to SvelteKit, Rabira</h1>
 <p class="text-base text-gray-400 p-4">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
