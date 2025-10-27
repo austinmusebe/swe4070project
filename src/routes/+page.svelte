aca2126849794e960d5213f9d8d26cc13478cf64
austinmusebe <austinmusebe@gmail.com>
2025-10-27 14:08:41 +0300

    Added the product card and some quantity management stuff.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
-	import Header from "../components/Header.svelte";
-
+	import Header from '../components/Header.svelte';
+	import ProductCard from '../components/ProductCard.svelte';
 </script>
-<Header/>
+
+<Header />
+<ProductCard />
 <h1 class="title">Welcome to SvelteKit, Rabira</h1>
-<p class="text-base text-gray-400 p-4">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p class="p-4 text-base text-gray-400">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
